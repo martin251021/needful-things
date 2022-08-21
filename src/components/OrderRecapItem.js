4ad56fb7a5d2774386dd708c47f56b6b0742b811
MartinJurkovic <martin.jurkovic94@gmail.com>
2022-08-21 13:02:53 +0200

    orderrecapitem fixed

--- src/components/OrderRecapItem.js
+++ src/components/OrderRecapItem.js
@@ -1,9 +1,15 @@
 import {Link} from "react-router-dom"
 
 export default function OrderRecapItem({id, title, image, price}) {
+
+    const styles = {
+        textDecoration: "none",
+        color: "black"
+    }
+
     return(
         <div className="order-recap">
-            <Link to={`${id}`} target="_blank">
+            <Link style={styles} to={`${id}`} target="_blank">
                 <div className="order-recap-img-title">
                     <img className="order-recap-item-img" src={image}/>
                     <p className="order-recap-item-title">{`${title.slice(0,20)}...`}</p>
