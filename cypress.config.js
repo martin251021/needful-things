c21db9539fd2a2cc8ecd7fc81f09d69c423b32f2
MartinJurkovic <martin.jurkovic94@gmail.com>
2022-09-18 15:25:59 +0200

    tests on singleproduct page implemented

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3000/",
-    excludeSpecPattern: "**/examples/*",
+    baseUrl: "http://localhost:3000/"  
   },
 });
---
+++ cypress/e2e/product.cy.js
@@ -0,0 +1,35 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
+
+
+describe("product page test", () => {
+
+  beforeEach(() => {
+    cy.visit(`http://localhost:3000/search/1`)
+  })
+
+  it("should check if the quantity is 1 and then check if its possible to go below 1", () => {
+    cy.get(".singleproduct-qty-input").should("have.value", 1)
+    cy.get(".singleproduct-qty-decrease").click()
+    cy.get(".singleproduct-qty-input").type("{backspace}")
+    cy.get(".singleproduct-qty-input").should("have.value", 1)
+  })
+
+  it("should check if the quantity adds properly", () => {
+    cy.get(".singleproduct-qty-input").should("have.value", 1)
+    cy.get(".singleproduct-qty-increase").click()
+    cy.get(".singleproduct-qty-input").should("have.value", 2)
+    cy.get(".singleproduct-qty-decrease").click()
+    cy.get(".singleproduct-qty-input").should("have.value", 1)
+  })
+
+  it("should check whether item added to cart displayed modal", () => {
+    cy.contains("Add to cart").click()
+    cy.get(".modal").should("be.visible")
+  })
+
+
+})
